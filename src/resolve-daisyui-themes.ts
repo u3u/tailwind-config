88b63aec3697b6b7d89e13325ed779d6bf713559
qwq <qwq@qwq.cat>
2023-12-20 10:37:18 +0800

    fix: call `require` on runtime

--- src/resolve-daisyui-themes.ts
+++ src/resolve-daisyui-themes.ts
@@ -1,11 +1,16 @@
 type Themes = typeof import('daisyui/src/theming/themes').default;
 
-export const resolveDaisyuiThemes = (): Themes => {
+export const resolveDaisyuiThemes = (): Partial<Themes> => {
   try {
     // v3+
     return require('daisyui/src/theming/themes');
   } catch {
     // v2
-    return require('daisyui/src/colors/themes');
+    try {
+      // "require" call with a try/catch block to handle this failure at run-time instead of bundle-time.
+      return require('daisyui/src/colors/themes');
+    } catch {
+      return {};
+    }
   }
 };
